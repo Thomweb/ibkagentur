dec4e534805675027778289f71b0b09bc7aab297
Thomweb <thom@thomweb.de>
2024-10-24 21:59:25 +0200

    Slider alle Seiten

--- Resources/Public/js/ibkfunctions.js
+++ Resources/Public/js/ibkfunctions.js
@@ -121,9 +121,24 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-    slidesPerView: 4,
+    slidesPerView: 1,
     slidesPerGroup: 1,
-    spaceBetween: 15,
+    spaceBetween: 0,
+
+    breakpoints: {
+        400: {
+            slidesPerView: 2,
+            spaceBetween: 5
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 5
+        },
+        992: {
+            slidesPerView: 4,
+            spaceBetween: 5
+        }
+    },
     speed: 500,
     autoplay: {
         delay: 2000,
